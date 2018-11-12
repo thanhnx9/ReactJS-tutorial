2b06ea497429809626e91a30ee29836453f7ebab
ThanhNX <xuanthanh1228@gmail.com>
2018-11-13 00:20:08 +0700

    vd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,44 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const a1="Thanh";
+class App1 extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Ten toi la: {a1} </h3>
+                <div className="form-check">
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" name id defaultValue="checkedValue" defaultChecked />
+                        Display value
+                    </label>
+                </div>
+            </div>
+        );
+    }
+}
+
+const so=[1,2,3,4,5,6]
+const so2=so.map((x)=>x*3)+';'
+const so3=so.map((x)=> (
+    <div>
+        <li> So: {x}</li>
+    </div>
+));
+class App3 extends Component {
+    render() {
+        return (
+            <div>
+                <a>{so2}</a>
+                <a>{so3}</a>
+            </div>
+        );
+    }
+}
+
+
+
 // Dinh nghia props cach 1
 function NumberOne(prop1) {
     return(
@@ -40,6 +78,8 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
+                    <App1/>
+                    <App3/>
                 </header>
                 <p className="App-intro">
                     Edit <code>src/App.js</code> and save to reload.
